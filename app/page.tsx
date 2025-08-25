8fe1cb7fcd8ed9d17e9c369483993900e737a893
Jorge <jsanchezpc@hotmail.com>
2025-08-25 21:21:36 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,27 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="font-sans my-4 rounded p-8 px-20">
+      <h1 className="text-5xl font-semibold mb-8">Noticias</h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nobis eaque id eos veniam, neque minima cumque accusamus nam dolore illum, perferendis tenetur ipsum dolorum! Numquam illum consequatur optio nesciunt.
+     Quo, id ratione nam atque, possimus reiciendis modi sit dicta saepe et voluptate reprehenderit? Nulla hic nemo, itaque in ipsam quas nihil adipisci temporibus? Voluptate voluptatum repudiandae velit ad quisquam.
+     Hic voluptatum minus accusamus odio alias at, expedita veritatis delectus explicabo esse aperiam assumenda est. Minima dignissimos a, tenetur dolore necessitatibus quos provident maxime alias reprehenderit dolorum repudiandae hic amet.
+     Error eum illo suscipit perspiciatis molestias necessitatibus, nobis beatae quia corporis eligendi quas provident, labore commodi blanditiis repellat officiis eos repudiandae, non cupiditate. Accusantium obcaecati asperiores saepe maxime nostrum repellat.
+     Temporibus dolore sed, nobis, magni dolorem inventore earum, voluptate quisquam repellendus sit possimus deserunt exercitationem. Accusantium veniam corporis voluptate necessitatibus, accusamus quos. Optio animi mollitia deserunt expedita. Deleniti, reiciendis? Distinctio.
+     Explicabo, quibusdam ullam? Nihil itaque ad facilis debitis autem, unde, mollitia reprehenderit deserunt eos recusandae accusantium tempore, temporibus doloribus explicabo vel? Provident eius aut nulla dolor ratione porro quidem laboriosam.
+     Voluptatem cumque sequi unde fuga! Nemo ipsum amet doloremque officiis quod sunt suscipit ea at rem neque dignissimos corrupti assumenda quam blanditiis repellendus sint fugit praesentium, impedit culpa maxime quas.
+     Mollitia illum eum tempore cumque quidem deserunt illo accusantium sed incidunt! Iste voluptate ullam nostrum totam numquam eaque itaque doloremque unde repellat omnis iure neque recusandae, quaerat sit temporibus aperiam?
+     Totam quos harum excepturi? Illum officia provident similique nemo cum aliquam aspernatur amet eligendi vero quo aliquid, delectus culpa facere laboriosam mollitia neque. Obcaecati, nulla dicta quis ipsa eligendi iste.
+     Fugiat aspernatur iusto iure a quibusdam odio molestias, libero alias enim repudiandae fugit eveniet, explicabo incidunt atque reiciendis itaque doloribus illo, ipsa aut! Facere ipsam perspiciatis aut non ipsum commodi.
+     Voluptas dolor dicta voluptatum doloremque odit, necessitatibus iste ratione error maxime, assumenda perferendis suscipit at ea illo, hic fuga officiis. Voluptates, provident. Reiciendis delectus vel possimus praesentium minima, ad eveniet.
+     Illum cupiditate a iure, voluptates repudiandae sunt quis eius iste ratione quas rerum totam aut, eaque dolorum eveniet harum quasi quos officia perspiciatis saepe commodi. Nihil quisquam doloremque ea illo!
+     Laudantium sit, architecto quibusdam minus laboriosam aperiam porro expedita? Error autem voluptatum libero modi dicta veritatis alias recusandae impedit architecto veniam nemo nobis, magnam saepe in quam, ipsam placeat cumque.
+     Eveniet dicta sint cupiditate fuga amet, nulla doloremque tenetur aliquid ex. Placeat provident necessitatibus cum voluptas ipsa vitae consequatur molestiae reprehenderit consequuntur soluta! Suscipit, natus? Maxime recusandae deserunt quaerat dolorem?
+     Adipisci, nam laboriosam. A voluptatibus ducimus reiciendis quam ipsam dicta. Incidunt odit magnam possimus consequatur eveniet rem aliquam animi explicabo fugiat necessitatibus dolore accusamus quia optio ut, assumenda doloribus nostrum.
+     Aut praesentium maiores nesciunt soluta delectus suscipit deserunt ex doloremque minus numquam odio, quae cupiditate. Deserunt officia recusandae sequi ducimus fuga soluta, libero harum nesciunt porro animi nemo quidem molestias!
+     Repellat sequi dolore laboriosam error delectus unde facilis, officiis amet sit explicabo cupiditate? Iure esse repellendus quod, alias natus accusamus. Deleniti odio soluta numquam animi sunt aliquam quidem quis vitae.
+     Excepturi minus minima incidunt similique magnam dignissimos iusto fuga ex vitae qui est culpa, provident soluta nisi velit unde autem eum, sed facere, ab sit sint. Numquam hic error molestiae.
+     Quibusdam sequi labore culpa fugit? Quas, iure? Eos similique ad aut labore doloribus, adipisci tenetur assumenda aspernatur magnam voluptate amet veniam vero fuga eius illo molestias dicta enim laborum dolor.
+     Minima neque, ducimus laboriosam omnis incidunt, necessitatibus modi nesciunt nam atque iure cumque? Odit deleniti numquam, itaque velit fugiat enim quia ipsum non. Odit, eaque et aliquam similique natus inventore?</p>
     </div>
   );
 }
---
+++ components/app-sidebar.tsx
@@ -0,0 +1,67 @@
+import { LandPlot, Home, Map, BusFront, Settings } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+  {
+    title: "Inicio",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Mapa",
+    url: "/mapa",
+    icon: Map,
+  },
+  {
+    title: "Planes",
+    url: "#",
+    icon: LandPlot,
+  },
+  {
+    title: "Transporte",
+    url: "#",
+    icon: BusFront,
+  },
+  {
+    title: "Ajustes",
+    url: "#",
+    icon: Settings,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Mataró</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
