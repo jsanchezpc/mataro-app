007e51bd03fb8772c61f1a87ea364fdf26e4448a
Jorge Sánchez <jsanchezpc@hotmail.com>
2025-08-26 00:14:43 +0200

    feat: changed to blue theme, added textarea on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
     <div className="font-sans my-4 rounded p-8 px-20">
-      <h1 className="text-4xl">Mataró</h1>
+      <div>
+        <Textarea placeholder="Dile a Mataró lo que pasa" className="resize-none min-h-20 h-auto max-h-50" />
+        <div className="flex w-full justify-end mt-2">
+          <Button className="text-white cursor-pointer">Publicar</Button>
+        </div>
+      </div>
+
     </div>
   );
 }
