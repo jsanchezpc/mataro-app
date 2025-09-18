7f5ce2fa27aeb71f9bcf7274fc9bb95e4c87cfd1
Jorge Sánchez <jsanchezpc@hotmail.com>
2025-09-18 04:13:12 +0200

    fix: post type fix

--- types/post.ts
+++ types/post.ts
@@ -1,14 +1,13 @@
 // types/post.ts
 // import { Timestamp } from "next/dist/server/lib/cache-handlers/types";
 export type Post = {
-    id: string;
-    author: string;
-    content: string;
-    uid: string;
-    // Firestore Timestamp se serializa a un objeto con _seconds y _nanoseconds
-    // o a una cadena ISO si se configura un serializador.
-    timestamp: number | { _seconds: number; _nanoseconds: number } | Date | string; // Ajustado
-    rt: number;
-    likes: number;
-    comments: number; 
-};
+  id: string
+  uid: string
+  author: string
+  content: string
+  timestamp: number
+  rt: number
+  likes: number
+  likedBy: string[]
+  comments: { id: string; text: string; author: string }[]
+}
