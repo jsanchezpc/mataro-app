e1e1fe1324edf33eded7e7a6cb9cfb009db8fe93
Jorge Sánchez <jsanchezpc@hotmail.com>
2025-12-14 01:50:46 +0100

    fix: hide unfinished views

---
+++ app/jobs/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function JobsPage() {
+    return (
+    <div className="font-sans rounded md:p-8">
+      <div className="max-w-200 mx-auto">
+        <h1 className="text-6xl font-light">Trabajos</h1>
+      </div>
+    </div>
+    )
+}
